966688c42709134783cb8cca781e58c45bff9847
AndyHine <62076750+AndyHine@users.noreply.github.com>
2021-03-13 08:51:08 +0200

    Add files via upload

---
+++ adp3git1/src/za/ac/cput/adp3git1/Ants.java
@@ -0,0 +1,13 @@
+package za.ac.cput.adp3git1;
+
+public class Ants {
+    /**
+     *
+     * @param str
+     * @return
+     */
+
+    public int lengthOfString(String str){
+        return str.length();
+    }
+}
